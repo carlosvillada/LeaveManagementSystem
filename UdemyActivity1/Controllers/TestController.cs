3ade7e6c8f1a89e53c452a5a1d8d770c0df03bcc
CarlosVillada <carlosvilladagonzalez@gmail.com>
2024-09-21 10:27:45 -0600

    Agregar archivos de proyecto.

---
+++ UdemyActivity1/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyActivity1.Models;
+
+namespace UdemyActivity1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(1954, 12, 01, 12, 23, 59)
+            };
+
+            return View(data);
+        }
+    }
+}
